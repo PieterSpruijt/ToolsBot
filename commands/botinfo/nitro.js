191a95b74d882a8dc74afbb66e99b22ae67876d4
Pieter Spruijt <mollyheerlen@gmail.com>
2021-08-11 17:02:54 +0200

    nitro joke

---
+++ commands/botinfo/nitro.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "nitro",
+    description: "Generate a free Nitro code!",
+    category: "botinfo",
+    run: async (bot, interaction) => {
+        interaction.editReply(`Free nitro for you: [https://discord.gift/Ip89s8hyFs6geFz0](https://www.youtube.com/watch?v=dQw4w9WgXcQ)`);
+    },
+}
