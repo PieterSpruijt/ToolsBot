cd378192c640f0184207817f228c4ab5eea69f41
Pieter Spruijt <mollyheerlen@gmail.com>
2021-08-11 17:12:01 +0200

    edited nitro and source commands

--- commands/botinfo/nitro.js
+++ commands/botinfo/nitro.js
@@ -3,6 +3,6 @@ module.exports = {
     description: "Generate a free Nitro code!",
     category: "botinfo",
     run: async (bot, interaction) => {
-        interaction.editReply(`Free nitro for you: [https://discord.gift/Ip89s8hyFs6geFz0](https://www.youtube.com/watch?v=dQw4w9WgXcQ)`);
+        interaction.editReply(`Free nitro for you: [<https://discord.gift/Ip89s8hyFs6geFz0>](<https://www.youtube.com/watch?v=dQw4w9WgXcQ>)`);
     },
 }
--- commands/botinfo/source.js
+++ commands/botinfo/source.js
@@ -1,8 +1,17 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: "source",
     description: "I'm open source GUYS!",
     category: "botinfo",
     run: async (bot, interaction) => {
-        interaction.editReply(`[click here to see the github](https://github.com/PieterSpruijt/ToolsBot)`);
+        const row = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageButton()
+                    .setLabel(`GitHub`)
+                    .setStyle(`LINK`)
+                    .setURL(`https://github.com/PieterSpruijt/ToolsBot`)
+            )
+        interaction.editReply({ content: `click on the button below to see the GitHub`, components: [row] });
     },
 }
