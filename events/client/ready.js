44400b48a3283ff97f3e6ec8006d1ce59fe03de5
Pieter Spruijt <mollyheerlen@gmail.com>
2021-08-11 17:54:05 +0200

    ready to offline

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,5 +4,11 @@ module.exports = async function (bot) {
     functions.loadCommands(bot).then(() => {
         console.log(`${bot.user.tag} is ready!`);
     });
+    setTimeout(() => {
+        bot.user.setStatus('invisible');
+    }, 5000);
+    setInterval(() => {
+        bot.user.setStatus('invisible');
+    }, 60000);
 
 }
